47d030ab64e4c8b1f5fcf314c10c9866f789d8ea
salsabila juandira <92494551+sjuandira@users.noreply.github.com>
2022-12-17 22:37:26 +0700

    Create ProductCounter.java

---
+++ ProductCounter.java
@@ -0,0 +1,13 @@
+package uapbo;
+
+/**
+ *
+ * @author saljuu
+ */
+public interface ProductCounter {
+    public final double TAX = 0.2;
+ 
+    public int hitungJumlahProduk();
+    
+    public int hitungHargaProduk();
+}
